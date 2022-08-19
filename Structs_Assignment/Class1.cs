d0ef28943cc804882796704347a888abf2c27309
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-19 10:09:48 +0100

    Add project files.

---
+++ Structs_Assignment/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs_Assignment
+{
+    public class Class1
+    {
+        public struct Number
+        {
+            public int Amount { get; set; }
+        }
+    }
+}
---
+++ Structs_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Structs_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Number number = new Class1.Number();
+
+            number.Amount = 5;
+
+            Console.WriteLine("Amount is: {0}", number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
